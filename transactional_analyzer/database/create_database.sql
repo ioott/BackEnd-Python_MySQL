cc5be26f4e9da9eec927b56b9cbbe22a4bdb9192
ioott <vioott@gmail.com>
2023-02-17 10:03:59 -0300

    fix: recria o banco para corrigir os tipos de dados

--- transactional_analyzer/database/create_database.sql
+++ transactional_analyzer/database/create_database.sql
@@ -1,13 +1,16 @@
 CREATE DATABASE dbcw;
 
-CREATE TABLE transactionals_data (
-  transaction_id VARCHAR(255) NOT NULL,
-  merchant_id VARCHAR(255) NOT NULL,
-  user_id VARCHAR(255) NOT NULL,
-  card_number VARCHAR(255) NOT NULL,
-  transaction_date DATE NOT NULL,
-  transaction_amount FLOAT NOT NULL,
-  device_id VARCHAR(255) NOT NULL,
-  has_cbk BOOL NOT NULL,
-  PRIMARY KEY (transaction_id)
-);
+USE dbcw;
+
+CREATE TABLE transactionals_data
+(
+transaction_id INT NOT NULL,
+merchant_id INT NOT NULL,
+user_id INT NOT NULL,
+card_number VARCHAR(255) NOT NULL,
+transaction_date DATETIME NOT NULL,
+transaction_amount DECIMAL(12,2) NOT NULL,
+device_id VARCHAR(255) NOT NULL,
+has_cbk CHAR(5) NOT NULL,
+PRIMARY KEY (transaction_id)
+)
