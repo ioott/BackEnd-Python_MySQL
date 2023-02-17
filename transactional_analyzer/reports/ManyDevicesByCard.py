5e26d91271e33d1862ee52fb37af27d09a2a288e
ioott <vioott@gmail.com>
2023-02-17 08:34:27 -0300

    refat: refatora para separar cada funcionalidade em uma função

--- transactional_analyzer/reports/ManyDevicesByCard.py
+++ transactional_analyzer/reports/ManyDevicesByCard.py
@@ -1,4 +1,5 @@
 from database.MySQLConnector import MySQLConnector
+from utils.export_csv import export_csv
 
 
 class ManyDevicesByCard:
@@ -11,33 +12,51 @@ class ManyDevicesByCard:
         self.conn = self.mysql.connection
         self.db_cursor = self.conn.cursor()
 
-        sub_query = (
+        results = make_report(self)
+        save_report(results)
+        print_report(results)
+
+        self.db_cursor.close()
+        return results
+
+
+def make_report(db_cursor):
+    sub_query = (
             "SELECT card_number "
             "FROM transactionals_data "
             "WHERE has_cbk = 'TRUE' "
             "GROUP BY card_number "
             "HAVING COUNT(DISTINCT device_id) > 1"
         )
-        query = (
-            "SELECT transaction_id, user_id, card_number, "
-            "transaction_amount, device_id, has_cbk "
-            "FROM transactionals_data "
-            f"WHERE card_number IN ({sub_query}) "
-            "ORDER BY card_number"
+    query = (
+        "SELECT transaction_id, user_id, card_number, "
+        "transaction_amount, device_id, has_cbk "
+        "FROM transactionals_data "
+        f"WHERE card_number IN ({sub_query}) "
+        "ORDER BY card_number"
+    )
+
+    db_cursor.execute(query)
+    return db_cursor.fetchall()
+
+
+def print_report(report):
+    print('\n >>> O relatório foi salvo na pasta "exported_reports" <<<\n')
+
+    for (transaction_id, user_id, card_number,
+            transaction_amount, device_id, has_cbk) in report:
+        print(
+            f'Id da transação: {transaction_id}, Id do Usuário: {user_id},'
+            f' Cartão: {card_number}, Valor: R$ {transaction_amount}, '
+            f'Id do Dispositivo: {device_id}, Houve Chargeback? {has_cbk}'
         )
 
-        self.db_cursor.execute(query)
-        results = self.db_cursor.fetchall()
+    print('\n >>> O relatório foi salvo na pasta "exported_reports" <<<\n')
 
-        for (transaction_id, user_id, card_number,
-                transaction_amount, device_id, has_cbk) in results:
-            print(
-                f'Id da transação: {transaction_id}, Id do Usuário: {user_id},'
-                f' Cartão: {card_number}, Valor: R$ {transaction_amount}, '
-                f'Id do Dispositivo: {device_id}, Houve Chargeback? {has_cbk}'
-            )
 
-        self.db_cursor.close()
+def save_report(report):
+    filepath = 'exported_reports/ManyDevicesByCard.csv'
+    export_csv(filepath, report)
 
 
 if __name__ == '__main__':
