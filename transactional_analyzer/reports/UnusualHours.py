50a025fac74643fe1df4104c360994d0b7868bee
ioott <vioott@gmail.com>
2023-02-17 08:35:04 -0300

    refat: refatora para separar cada funcionalidade em uma função

--- transactional_analyzer/reports/UnusualHours.py
+++ transactional_analyzer/reports/UnusualHours.py
@@ -1,4 +1,5 @@
 from database.MySQLConnector import MySQLConnector
+from utils.export_csv import export_csv
 
 
 class UnusualHours:
@@ -11,7 +12,16 @@ class UnusualHours:
         self.conn = self.mysql.connection
         self.db_cursor = self.conn.cursor()
 
-        query = (
+        results = make_report(self)
+        save_report(results)
+        print_report(results)
+
+        self.db_cursor.close()
+        return results
+
+
+def make_report(db_cursor):
+    query = (
             "SELECT * "
             "FROM transactionals_data "
             "WHERE has_cbk = 'TRUE' "
@@ -19,18 +29,31 @@ class UnusualHours:
             "ORDER BY transaction_amount DESC "
         )
 
-        self.db_cursor.execute(query)
-        results = self.db_cursor.fetchall()
+    db_cursor.execute(query)
+    return db_cursor.fetchall()
 
-        for (transaction_id, merchant_id, user_id,
-                card_number, transaction_date, transaction_amount,
-                device_id, has_cbk) in results:
-            print(
-                f'Id da transação: {transaction_id}, '
-                f'Id do Vendedor: {merchant_id}, Id do Usuário: {user_id}, '
-                f'Cartão: {card_number}, Data: {transaction_date}, '
-                f'Valor: R$ {transaction_amount}, '
-                f'Id do Dispositivo: {device_id}, Houve Chargeback? {has_cbk}'
-            )
 
-        self.db_cursor.close()
+def print_report(report):
+    print('\n >>> O relatório foi salvo na pasta "exported_reports" <<<\n')
+
+    for (transaction_id, merchant_id, user_id,
+            card_number, transaction_date, transaction_amount,
+            device_id, has_cbk) in report:
+        print(
+            f'Id da transação: {transaction_id}, '
+            f'Id do Vendedor: {merchant_id}, Id do Usuário: {user_id}, '
+            f'Cartão: {card_number}, Data: {transaction_date}, '
+            f'Valor: R$ {transaction_amount}, '
+            f'Id do Dispositivo: {device_id}, Houve Chargeback? {has_cbk}'
+        )
+
+    print('\n >>> O relatório foi salvo na pasta "exported_reports" <<<\n')
+
+
+def save_report(report):
+    filepath = 'exported_reports/UnusualHours.csv'
+    export_csv(filepath, report)
+
+
+if __name__ == '__main__':
+    UnusualHours().report()
